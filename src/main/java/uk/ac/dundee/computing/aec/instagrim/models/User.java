a087f803dc63ce553d8dd5517e79ea6cc9c65b9a
Toysif <rangersfc_2007@hotmail.co.uk>
2016-10-23 22:16:05 +0100

    extends registration. first name added to database

--- src/main/java/uk/ac/dundee/computing/aec/instagrim/models/User.java
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/models/User.java
@@ -27,7 +27,7 @@ public class User {
         
     }
     
-    public boolean RegisterUser(String username, String Password){
+    public boolean RegisterUser(String username, String Password, String firstname){
         AeSimpleSHA1 sha1handler=  new AeSimpleSHA1();
         String EncodedPassword=null;
         try {
@@ -37,12 +37,12 @@ public class User {
             return false;
         }
         Session session = cluster.connect("instagrim");
-        PreparedStatement ps = session.prepare("insert into userprofiles (login,password) Values(?,?)");
+        PreparedStatement ps = session.prepare("insert into userprofiles (login,password,first_name) Values(?,?,?)");
        
         BoundStatement boundStatement = new BoundStatement(ps);
         session.execute( // this is where the query is executed
                 boundStatement.bind( // here you are binding the 'boundStatement'
-                        username,EncodedPassword));
+                        username,EncodedPassword,firstname));
         //We are assuming this always works.  Also a transaction would be good here !
         
         return true;
